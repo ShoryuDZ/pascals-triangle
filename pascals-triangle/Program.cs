4ed6cfafb0e96142a75a8b3d03d934670ce12e83
Shoryu Das-Zaman <58425525+ShoryuDZ@users.noreply.github.com>
2020-09-13 23:03:59 +1000

    Initial Pyramid Setup
    Getting the initial pyramid shape with 1 digit numbers

--- pascals-triangle/Program.cs
+++ pascals-triangle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace pascalstriangle
 {
@@ -6,7 +7,52 @@ namespace pascalstriangle
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Pascal's Triangle generator");
+            Console.Write("How many lines of the trangle do you want? ");
+            String lineInput = Console.ReadLine();
+            Console.WriteLine("Okay, printing " + lineInput + " lines");
+
+            int numberOfLines = int.Parse(lineInput);
+            int width = 2 * numberOfLines - 1;
+
+            List<List<int>> lines = new List<List<int>>();
+            for (int counter = 0; counter < numberOfLines; counter++)
+            {
+                if (counter == 0)
+                {
+                    lines.Insert(counter, new List<int>{ 1 });
+                }
+
+                else if (counter == 1)
+                {
+                    lines.Insert(counter, new List<int>{ 1, 1 });
+                }
+
+                else
+                {
+                    List<int> newLine = new List<int>{};
+                    for (int lineCounter = 0; newLine.Count <= counter; lineCounter ++)
+                    {
+                        newLine.Insert(lineCounter, lineCounter + 1);
+                    }
+                    lines.Insert(counter, newLine);
+                }
+
+                String line = format(lines[counter]);
+                Console.WriteLine(line.ToString().PadLeft(numberOfLines + counter, ' '));
+            }
+
+            String format(List<int> line)
+            {
+                String lineString = "";
+
+                foreach (int value in line)
+                {
+                    lineString += value.ToString() + " ";
+                }
+
+                return lineString.Trim();
+            }
         }
     }
 }
